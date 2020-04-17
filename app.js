432daf3a7b42688ee9fd3a2c3c499f378890c39c
Tyler <thompson92081@gmail.com>
2020-04-17 14:25:23 -0700

    added functions

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 //global variables
 const compChoicesArray = ["r", "p", "s"];
 let compChoice
+let userChoice
 
 // prompt user to input r p or s.
-let userChoice = prompt("choose R, P or S");
-userChoice = userChoice.toLowerCase();
-// if they type something else prompt again for r p or s. once a selection is made have computer randomly generate either r p or s.
-if (userChoice !== "r" && userChoice !== "p" && userChoice !== "s") {
-    alert("pick a either r, p , or s")
-} else {
+function getUserChoice() {
+    userChoice = prompt("choose R, P or S");
+    userChoice = userChoice.toLowerCase();
+    validateChoice();
+};
+
+function generateComputerChoice() {
     const randomArrayPosition = Math.floor(Math.random() * 3)
     compChoice = compChoicesArray[randomArrayPosition]
     console.log(compChoice)
 }
+
+// if they type something else prompt again for r p or s. once a selection is made have computer randomly generate either r p or s.
+function validateChoice() {
+    if (userChoice !== "r" && userChoice !== "p" && userChoice !== "s") {
+        alert("pick a either r, p , or s")
+        getUserChoice();
+    } else {
+        generateComputerChoice();
+        compareAnswer();
+    }
+};
+
+//then compare users selection to computers and output either win lose or draw.
+function compareAnswer() {
+    if (userChoice === compChoice) {
+        alert("Tie!")
+    } else if ((userChoice === "r" && compChoice === "s") || (userChoice === "s" && compChoice === "p") || (userChoice === "p" && compChoice === "r")) {
+        alert("Winner!")
+    }
+    else {
+        alert("Loser!")
+    }
+};
+getUserChoice()
 console.log("User Choice: " + userChoice)
 console.log("Comp Choice: " + compChoice)
-//then compare users selection to computers and output either win lose or draw.
-if (userChoice === compChoice) {
-    alert("Tie!")
-} else if ((userChoice === "r" && compChoice === "s") || (userChoice === "s" && compChoice === "p") || (userChoice === "p" && compChoice === "r")) {
-    alert("Winner!")
-}
-else {
-    alert("Loser!")
-}
